801de31647d42133002183f3710d394eba9ef719
David Alonso <davidalonso991@gmail.com>
2024-10-26 18:14:28 -0600

    Agregado de router y modelo de carrito y agregado de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "./cors.js";
+
+/*Routers */
+import carritoRouter from "./routes/carrito.routes.js";
 
 dotenv.config();
 const App = express();
 
 App.use(express.json());
+App.use(cors);
+
+/* Routes */
+App.use("/carrito", carritoRouter);
 
 export default App;
---
+++ src/routes/carrito.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const carritoRouter = Router();
+
+carritoRouter.get("/", (req, res) => {});
+carritoRouter.get("/:id", (req, res) => {});
+carritoRouter.post("/", (req, res) => {});
+carritoRouter.delete("/:id", (req, res) => {});
+carritoRouter.put("/:id", (req, res) => {});
+
+export default carritoRouter;
